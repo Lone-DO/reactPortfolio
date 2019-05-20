05d69b9502e3d23f187e716fff54bc993298c86d
lone-DO <lotussythe@gmail.com>
2019-05-20 16:16:59 +0200

    Setting up Audio Que for AC App

--- client/src/components/projects/_actunes/_clock.js
+++ client/src/components/projects/_actunes/_clock.js
@@ -13,7 +13,17 @@ class Clock extends Component {
   render() {
     this.renderContent();
 
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <audio controls loop autoplay="true">
+          <source
+            src="http://23.237.126.42/ost/animal-crossing-full-gamerip/xsusxbduxb/078%20-%203%20AM%20%28Cherry%29.mp3"
+            type="audio/mp3"
+          />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    );
   }
 }
 
