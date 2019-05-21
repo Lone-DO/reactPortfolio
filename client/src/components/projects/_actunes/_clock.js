e6e40859023d9e7498e52d8c7a290bfb1df8fa5d
lone-DO <lotussythe@gmail.com>
2019-05-21 23:44:48 +0200

    Audio Player checkpoint

--- client/src/components/projects/_actunes/_clock.js
+++ client/src/components/projects/_actunes/_clock.js
@@ -5,9 +5,12 @@ class Clock extends Component {
   constructor() {
     super();
     this.state = {
-      hours: "00",
-      minutes: "00",
-      seconds: "00"
+      period: "AM",
+      isloaded: false,
+      date: new Date(),
+      hours: new Date().getHours(),
+      minutes: new Date().getMinutes(),
+      seconds: new Date().getSeconds()
     };
     this.timer = this.timer.bind(this);
   }
@@ -15,7 +18,9 @@ class Clock extends Component {
   timer() {
     setInterval(() => {
       const prepend = value => (value = value < 10 ? `0${value}` : value);
+      let { hours, minutes, seconds, period, isloaded } = this.state;
       let date = new Date();
+      let prevHour = this.state.date.getHours();
       let currentMinute = date.getMinutes().toString();
       let currentSecond = date.getSeconds().toString();
 
@@ -24,14 +29,22 @@ class Clock extends Component {
           ? (date.getHours() - 12).toString()
           : date.getHours().toString();
 
-      if (this.state.hours !== currentHour) {
+      if (hours !== currentHour) {
         this.setState({ hours: prepend(currentHour) });
       }
-      if (this.state.minutes !== currentMinute) {
+      if (minutes !== currentMinute) {
         this.setState({ minutes: prepend(currentMinute) });
       }
-      if (this.state.seconds !== currentSecond) {
+      if (seconds !== currentSecond) {
         this.setState({ seconds: prepend(currentSecond) });
+      }
+      if (prevHour > 12) {
+        this.setState({ period: "PM" });
+      } else if (prevHour < 12 && period !== "AM") {
+        this.setState({ period: "AM" });
+      }
+      if (!isloaded) {
+        this.setState({ isloaded: true });
       }
     }, 1000);
   }
@@ -39,23 +52,26 @@ class Clock extends Component {
   componentDidMount() {
     this.timer();
   }
+
   renderContent() {
     return [
       <Player
+        key="musicPlayer"
         audioData={this.props.audioData}
         videoData={this.props.videoData}
         time={this.state}
       />,
-      <div>
+      <div key="time">
         <i>{this.state.hours}</i>
         <i>:{this.state.minutes}</i>
         <i>:{this.state.seconds}</i>
+        <i>{this.state.period}</i>
       </div>
     ];
   }
 
   render() {
-    return <div>{this.renderContent()}</div>;
+    return this.renderContent();
   }
 }
 
--- client/src/components/projects/_actunes/albums/_player.js
+++ client/src/components/projects/_actunes/albums/_player.js
@@ -1,24 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Player = ({ audioData, customData = ["Not Ready"], videoData }) => {
-  console.log(audioData.Original.Normal["1PM"]);
+class Player extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasStarted: false,
+      weather: "Normal",
+      song: ""
+    };
+    this.loadSong = this.loadSong.bind(this);
+  }
 
-  // document.selectQuery("player").src = audioData.Origial.Normal;
-  const loadSong = () => {
-    //
-  };
-  return [
-    <audio controls loop autoPlay className="player" key="1">
-      <source
-        src="http://23.237.126.42/ost/animal-crossing-full-gamerip/xsusxbduxb/078%20-%203%20AM%20%28Cherry%29.mp3"
-        type="audio/mp3"
-      />
-      Your browser does not support the audio element.
-    </audio>,
-    <button key="2" onClick={loadSong}>
-      Change Audio
-    </button>
-  ];
-};
+  loadSong(playlist) {
+    let { hours, period } = this.props.time;
+    let { audioData } = this.props;
+    let title =
+      hours < 10 && hours.indexOf("0") > -1
+        ? hours.replace("0", "") + period
+        : hours + period;
+
+    if (playlist === undefined && !this.state.hasStarted) {
+      this.setState({
+        hasStarted: true,
+        song: audioData.Original[this.state.weather][title]
+      });
+    } else if (playlist !== undefined) {
+      this.setState({
+        song: audioData[playlist][this.state.weather][title]
+      });
+    }
+
+    document.getElementById("player").load();
+  }
+
+  componentDidMount() {}
+  renderContent() {
+    if (this.props.time.isloaded) {
+      return [
+        <audio
+          controls
+          loop
+          autoPlay
+          id="player"
+          className="player"
+          key="player"
+        >
+          <source
+            src={this.state.song}
+            type="audio/mp3"
+            className="audioSource"
+          />
+          Your browser does not support the audio element.
+        </audio>,
+        <div key="songDials">
+          <p>{this.state.song}</p>
+          <button onClick={() => this.loadSong("Original")}>Orignal</button>
+          <button onClick={() => this.loadSong("CityFolk")}>City Folk</button>
+          <button onClick={() => this.loadSong("NewLeaf")}>NewLeaf</button>
+        </div>
+      ];
+    } else {
+      return "Loading Audio...";
+    }
+  }
+
+  render() {
+    return this.renderContent();
+  }
+}
 
 export default Player;
