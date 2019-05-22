5684fdfa688d55299ca4dab7bc706e557861ac8d
lone-DO <lotussythe@gmail.com>
2019-05-22 17:45:25 +0200

    Add Weather API

--- client/src/components/projects/_actunes/albums/_player.js
+++ client/src/components/projects/_actunes/albums/_player.js
@@ -6,19 +6,53 @@ class Player extends Component {
     this.state = {
       hasStarted: false,
       weather: "Normal",
+      weatherData: {},
+      weatherKey: "c818f18fb44cfccea6436940f6cea5f8",
       song: ""
     };
     this.loadSong = this.loadSong.bind(this);
+    this.loadWeather = this.loadWeather.bind(this);
   }
 
-  loadSong(playlist) {
+  loadWeather(location = "Hessen", unit = "metric") {
+    const key = "c818f18fb44cfccea6436940f6cea5f8";
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${key}`;
+    let updated = console.log("weather updated to: ", this.state.weather);
+    try {
+      fetch(url)
+        .then(res => res.json())
+        .then(res => this.setState({ weatherData: res }))
+        .then(res => {
+          let { weatherData } = this.state;
+          let message = `it is currently ${weatherData.main.temp} celcius in ${
+            weatherData.name
+          }`;
+          console.log(weatherData);
+
+          switch (weatherData.weather.main) {
+            case "Rain":
+              return this.setState({ weather: "Rain" });
+            case "Snow":
+              return this.setState({ weather: "Winter" });
+            default:
+              return this.setState({ weather: "Normal" });
+          }
+          // eslint-disable-next-line
+          updated();
+          alert(message);
+        });
+    } catch (err) {
+      alert(err, "Please try again...");
+    }
+  }
+
+  loadSong(playlist, weather = this.state.weather) {
     let { hours, period } = this.props.time;
     let { audioData } = this.props;
     let title =
       hours < 10 && hours.indexOf("0") > -1
         ? hours.replace("0", "") + period
         : hours + period;
-
     if (title === "12PM") {
       title = "Noon";
     } else if (hours === "00") {
@@ -31,9 +65,6 @@ class Player extends Component {
           song: audioData.Original[this.state.weather][title],
           hasStarted: true
         });
-        if (this.state.song === "") {
-        }
-        console.log(this.state.song);
       } catch (err) {
         alert(err.message);
       }
@@ -53,7 +84,9 @@ class Player extends Component {
     }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.loadWeather();
+  }
 
   renderContent() {
     // this.loadSong();
