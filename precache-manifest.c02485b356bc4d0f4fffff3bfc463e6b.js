3789431d6c8ccce5b59ad4c70c30eaeda8f5da0b
lone-DO <lotussythe@gmail.com>
2019-06-19 16:26:37 +0200

    Updates

---
+++ precache-manifest.c02485b356bc4d0f4fffff3bfc463e6b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cd05cb178babe89d00544beaf3724a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b777c7c45bcb2fcc510",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "6059ccbd515f35fb8a0f",
+    "url": "/static/css/main.830c5d9c.chunk.css"
+  },
+  {
+    "revision": "3b777c7c45bcb2fcc510",
+    "url": "/static/js/2.583c9c29.chunk.js"
+  },
+  {
+    "revision": "6059ccbd515f35fb8a0f",
+    "url": "/static/js/main.c4833ced.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8475561c5e08ec9d7376a317d2b28d5c",
+    "url": "/static/media/Animal_Crossing_Logo.8475561c.png"
+  },
+  {
+    "revision": "6b70462406ed8904ac16f86e52a90043",
+    "url": "/static/media/Banner--large-HD.6b704624.jpg"
+  },
+  {
+    "revision": "bb7637538dd09d9701ca2183fb18adc5",
+    "url": "/static/media/Banner--large.bb763753.jpg"
+  },
+  {
+    "revision": "0e49b6d1cecb9904c92e290df2f4c03a",
+    "url": "/static/media/Banner--medium-HD.0e49b6d1.jpg"
+  },
+  {
+    "revision": "3a54fafc2ce35d089b67ab1dc6bf8783",
+    "url": "/static/media/Banner--medium.3a54fafc.jpg"
+  },
+  {
+    "revision": "058b87c393359dd744a6227491e39def",
+    "url": "/static/media/Banner--mobile.058b87c3.jpg"
+  },
+  {
+    "revision": "a712aa817890a7fcdd93b38609f857ef",
+    "url": "/static/media/Banner--small-HD.a712aa81.jpg"
+  },
+  {
+    "revision": "e7f5136482d20a84396f7eb972307bc2",
+    "url": "/static/media/Banner--small.e7f51364.jpg"
+  },
+  {
+    "revision": "7e678cd8313e46230aaf0c2b209aa596",
+    "url": "/static/media/FerroRosso.7e678cd8.ttf"
+  },
+  {
+    "revision": "564ec00b262da7659294e01da6fde807",
+    "url": "/static/media/loading.564ec00b.gif"
+  },
+  {
+    "revision": "70daefe84ce73b49df194e7b9c3080cb",
+    "url": "/static/media/texturedBG4.70daefe8.png"
+  },
+  {
+    "revision": "6839837eba7bf81fdfd32dabacefcc58",
+    "url": "/static/media/weather-normal.6839837e.svg"
+  },
+  {
+    "revision": "b7e6dc1148fb62ebcfa3ad206cb59088",
+    "url": "/static/media/weather-raining.b7e6dc11.svg"
+  },
+  {
+    "revision": "27113c197c7cf4579ab93239834d9c15",
+    "url": "/static/media/weather-winter.27113c19.svg"
+  }
+]);
