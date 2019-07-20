ba6b48f5916841bb3f55d05ed0dc3f43a1c300e4
lone-DO <lotussythe@gmail.com>
2019-07-20 04:49:00 -0400

    Add Credits for inspiration

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -20,6 +20,16 @@ const Contact = () => {
         </a>
         <br />
       </address>
+
+      <p className="credits">
+        Credits to "Mako - Coyote" for the artistic inspiration <br />
+        All Rights reserved, check out the creator below <br />
+        <a href="https://www.youtube.com/watch?v=i0v6xKe2M0Q">Music Video,</a>
+        <br />
+        <a href="https://www.youtube.com/channel/UClpog0YxmljvL01cnQ1BeLA">
+          Artist - wearemako
+        </a>
+      </p>
     </article>
   );
 };
