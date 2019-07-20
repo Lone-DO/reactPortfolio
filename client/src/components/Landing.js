e60faa8a90aa205a227f347a9fa9ef9527d67749
lone-DO <lotussythe@gmail.com>
2019-07-20 05:17:50 -0400

    patch - Adjusted modal design

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,12 +2,19 @@ import React from "react";
 
 import About from "./About";
 import Projects from "./Projects";
+import Contact from "./Contact";
 
 const Landing = props => {
   return (
     <main className="App_content">
       <About />
       <Projects modal={props.modal} />
+      <button
+        className="btn --transparent --wide open-modal"
+        onClick={() => props.modal(<Contact />)}
+      >
+        Contact Me
+      </button>
     </main>
   );
 };
