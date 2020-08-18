5c047056035701f6cd5928fc0b38f284a533f87b
lone-DO <lotussythe@gmail.com>
2020-08-18 07:36:11 -0400

    remove unused log

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -4,50 +4,52 @@ import appData from "./projects/appData";
 import ProjectModel from "./_projectModel";
 
 class Projects extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      apps: appData
-    };
-  }
+   constructor(props) {
+      super();
+      this.state = {
+         apps: appData,
+      };
+   }
 
-  toggleElement(_class, _target) {
-    let elem = document.querySelector(`${_class} ${_target}`);
-    if (elem.style.display === "block") {
-      elem.style.display = "none";
-    } else {
-      elem.style.display = "block";
-    }
-  }
+   toggleElement(_class, _target) {
+      let elem = document.querySelector(`${_class} ${_target}`);
+      if (elem.style.display === "block") {
+         elem.style.display = "none";
+      } else {
+         elem.style.display = "block";
+      }
+   }
 
-  render() {
-    console.log();
-    const Applications = this.state.apps.map(app => {
+   render() {
+      const Applications = this.state.apps.map((app) => {
+         return (
+            <ProjectModel
+               modal={this.props.modal}
+               key={app.title}
+               title={app.title}
+               name={app.name}
+               headline={app.headline}
+               description={app.description}
+               launch={app.launch}
+               github={app.github}
+               modules={app.modules}
+               type={app.type}
+               toggle={this.toggleElement}
+            />
+         );
+      });
+
       return (
-        <ProjectModel
-          modal={this.props.modal}
-          key={app.title}
-          title={app.title}
-          name={app.name}
-          headline={app.headline}
-          description={app.description}
-          launch={app.launch}
-          github={app.github}
-          modules={app.modules}
-          type={app.type}
-          toggle={this.toggleElement}
-        />
+         <div
+            className="App_content_projects"
+            data-matching-link="#project-link"
+         >
+            <h2 className="App_content-title">List of personal applications</h2>
+            {Applications}
+            <hr></hr>
+         </div>
       );
-    });
-
-    return (
-      <div className="App_content_projects" data-matching-link="#project-link">
-        <h2 className="App_content-title">List of personal applications</h2>
-        {Applications}
-        <hr></hr>
-      </div>
-    );
-  }
+   }
 }
 
 export default Projects;
